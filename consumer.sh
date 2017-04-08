9d57752fc483cf4965b75e50dfe093f074c2ff9e
Isaac Diamond <isaacd9@gmail.com>
2017-04-08 15:51:10 -0700

    Moooooooooooooooooooo

--- consumer.sh
+++ consumer.sh
@@ -1,3 +1,7 @@
+
+from_user=$1;
+echo "Hi $from_user! MOOOOOOOOOOOOOOOOOO";
+
 while read meth
 do
   if [[ $meth =~ "put" ]]; then
@@ -7,7 +11,18 @@ do
     from=$(echo $data | jq -r ".from");
     contents=$(echo $data | jq -r ".contents");
 
-    echo "$from:
-    $contents" | cowsay -n
+    if [[ $contents != "null" ]];  then
+      echo "$from:
+      $contents" | cowsay -n
+    fi
+  elif [[ $meth =~ "@" ]]; then
+    msg=$(echo $meth | cut -d "@" -f 2)
+    to_user=$(echo $msg | cut -d " " -f 1 | tr '[:upper:]' '[:lower:]')
+    contents=$(echo $msg | cut -d " " -f 2-)
+
+    echo "$to_user:
+    $contents" | cowsay -n -f ./reverse.cow
+    curl -s -X POST -d "{ \"from\": \"$from_user\", \"contents\": \"$contents\" }" \
+      "https://cowchat-bd025.firebaseio.com/users/$to_user/messages.json" > /dev/null
   fi
-done < "${1:-/dev/stdin}"
+done < /dev/stdin
--- cowchat.sh
+++ cowchat.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-curl -sL -N -H "Accept: text/event-stream" https://cowchat-bd025.firebaseio.com/users/isaac/messages.json | ./consumer.sh
+me=$(echo $1 | tr '[:upper:]' '[:lower:]');
+
+(curl -sL -N -H "Accept: text/event-stream" https://cowchat-bd025.firebaseio.com/users/$me/messages.json & cat /dev/tty) | ./consumer.sh $1
 
